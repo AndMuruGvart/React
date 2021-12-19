6ba9921d126abb0c8cc0dd98f77f49928ba8f7d8
Murashev Andrey <shshk777@yandex.ru>
2021-12-20 02:02:32 +0500

    7th homework

---
+++ 7/src/server/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import ReactDOM from 'react-dom/server';
+// import { Header } from "../shared/Header.tsx";
+import { App } from "../App";
+import { indexTemplate } from "./indexTemplate.js";
+
+const app=express();
+
+app.use('/static', express.static('./dist/client'));
+
+app.get('/', (req, res) => {
+    res.send(
+        indexTemplate(ReactDOM.renderToString(App())),
+
+    );
+});
+
+app.get('/auth', (req, res) => {
+    res.send(
+        indexTemplate(ReactDOM.renderToString(App())),
+
+    );
+});
+
+app.listen(3000, () => {
+    console.log('Server started on http://localhost:3000');
+});
